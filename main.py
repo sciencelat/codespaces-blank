83269ce25e043410646b66a8038c95196cd8b4f9
lat <latifatogungbo@gmail.com>
2025-09-19 21:38:30 +0000

    added more options; lets customer see menu first

--- main.py
+++ main.py
@@ -2,3 +2,16 @@ print("Hello, welcome to the ordering food Chatbot")
 
 name = input("What is your name? ")
 age = input("Hello " + name + ", how old are you? ")
+
+choice = input("Would you like to check the menu first or just order? (menu/order): ")
+
+if choice.lower() == "menu":
+    print("Here's our menu:")
+    print("- Pizza")
+    print("- Burger")
+    print("- Salad")
+    print("- Pasta")
+
+food = input("What would you like to order? ")
+print("Great choice! One " + food + " coming right up.")
+print("Thanks for ordering, " + name + "!")
